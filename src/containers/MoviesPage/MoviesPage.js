92b7366ad1a59471517a54d9735bb9564f68b7f6
Alexey Musienko <musaleex@gmail.com>
2018-07-21 14:14:43 +0300

    fix pages redirection

---
+++ src/containers/MoviesPage/MoviesPage.js
@@ -0,0 +1,110 @@
+import React, { PureComponent } from 'react';
+import axios from '../../axios-config';
+import Movies from '../../components/Movies/Movies';
+import SearchBar from '../../components/SearchBar/SearchBar';
+import debounce from '../../debounce';
+import Spinner from '../../components/Spinner/Spinner';
+
+const key = '?api_key=daff65c036673bf55ddef790e717e11d';
+
+class MoviesPage extends PureComponent {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            data: null,
+            loading: true,
+        }
+
+        this.inputRef = React.createRef();
+        this.updateData = debounce(this.updateData, 700);
+    }
+    
+    
+    componentDidMount() {
+        console.log('[DidMount]', this.props)
+        let {queryName, id} = this.props.match.params;
+        let path = queryName ? '/search/' + encodeURI(queryName) : '/popular';
+        let url = (queryName ? '/search/movie' + key + '&query=' + encodeURI(queryName) : '/movie/popular' + key);
+        if (id > 1000) {
+            this.props.history.replace('/movies' + path + '/page/1000');
+            this.setState({
+                loading: true,
+            })
+            return;
+        }
+
+        axios.get(url + '&page=' + id)
+            .then(response => {
+                let pages  = response.data.total_pages;
+                if (id > pages) {
+                    this.props.history.replace(`/movies${path}/page/${pages-1}`);
+                    this.setState({
+                        loading: true,
+                    })
+                } else {
+                    this.setState({
+                        data: response.data,
+                        loading: false,
+                    })
+                }
+            })
+    }
+
+
+    componentDidUpdate() {
+        console.log('[DidUpdate]', this.props);
+        this.updateData()
+    }
+
+    changeHandler = (e) => {
+        this.props.history.push('/movies/search/' + encodeURI(e.target.value) + '/page/1')
+    }
+
+    updateData() {
+        let {queryName, id} = this.props.match.params;
+        let path = queryName ? '/search/' + encodeURI(queryName) : '/popular';
+        console.log(queryName, encodeURI(queryName))
+        let url = (queryName ? '/search/movie' + key + '&query=' + encodeURI(queryName) : '/movie/popular' + key);
+        if (id > 1000) {
+            this.props.history.replace('/movies/' + path + '/1000');
+            return;
+        }
+            axios.get(url + '&page=' + id)
+                .then(response => {
+                    console.log(response)
+                    const toString = (arr) => arr.map(e=>e.title).join('-_-');
+                    if (!this.state.data || (this.state.data && toString(response.data.results) !== toString(this.state.data.results))) {
+                        this.setState({
+                            data: response.data,
+                            loading: false,
+                        })
+                    }
+                }) 
+    }
+
+    render() {
+        console.log('Loading = ', this.state.loading)
+
+        let movies = (this.state.data && this.state.data.results) || null;
+        return (
+            <React.Fragment>
+                <SearchBar
+                    myRef = {el => this.inputRef = el}
+                    change = {this.changeHandler} 
+                    val = {this.props.match.params.queryName || ''}
+                />
+                {!this.state.loading ? movies.length ? <Movies data = {movies}/> : 'No search results' : <Spinner />}
+            </React.Fragment>
+        );
+    }
+}
+
+// function getPage(page) {
+//     axios.get('/movie/popular' + key + '&page=' + page)
+//         .then(response => {
+//             console.log(response.data)
+//         })
+// }
+
+export default MoviesPage;
