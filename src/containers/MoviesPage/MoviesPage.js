d2f73c8441dbc44a57459d6dea348977928d251a
Alexey Musienko <musaleex@gmail.com>
2018-07-24 16:58:49 +0300

    lifecycle hooks reworked

--- src/containers/MoviesPage/MoviesPage.js
+++ src/containers/MoviesPage/MoviesPage.js
@@ -25,46 +25,7 @@ class MoviesPage extends PureComponent {
     
     componentDidMount() {
         console.log('[DidMount]', this.props)
-        let {queryName, pageNumber} = this.props.match.params;
-        let path = queryName ? '/search/' + encodeURI(queryName) : '/popular';
-        let url = (queryName ? '/search/movie' + key + '&query=' + encodeURI(queryName) : '/movie/popular' + key);
-        if (pageNumber > 1000) {
-            this.props.history.replace('/movies' + path + '/page/1000');
-            this.setState({
-                loading: true,
-            })
-            return;
-        }
-
-        axios.get(url + '&page=' + pageNumber)
-            .then(response => {
-                let pages  = response.data.total_pages;
-                if (pageNumber > pages) {
-                    this.props.history.replace(`/movies${path}/page/${pages-1}`);
-                    this.setState({
-                        loading: true,
-                    })
-                } else {
-                    console.log(response)
-                    let movieList = response.data;
-                    axios.get('/genre/movie/list' + key)
-                        .then(genreResponse => {
-                            let genreNames = genreResponse.data.genres;
-                            for (let movie of movieList.results) {
-                                movie.genres = movie.genre_ids.map(
-                                    id => genreNames.find(
-                                        genre => genre.id === id
-                                    ).name
-                                )
-                            }
-
-                            this.setState({
-                                data: movieList,
-                                loading: false,
-                            })
-                        })
-                }
-            })
+        this.updateData();
     }
 
 
@@ -85,41 +46,56 @@ class MoviesPage extends PureComponent {
     }
 
     updateData() {
+        console.log('updating...')
         let {queryName, pageNumber} = this.props.match.params;
         let path = queryName ? '/search/' + encodeURI(queryName) : '/popular';
         let url = (queryName ? '/search/movie' + key + '&query=' + encodeURI(queryName) : '/movie/popular' + key);
         if (pageNumber > 1000) {
-            this.props.history.replace('/movies/' + path + '/1000');
+            // console.log('[Path by redirect]', '/movies' + path + '/page/1000')
+            this.props.history.replace('/movies' + path + '/page/1000');
+            this.setState({
+                loading: true,
+            })
             return;
         }
             axios.get(url + '&page=' + pageNumber)
                 .then(response => {
                     const toString = (arr) => arr.map(e=>e.title).join('-_-');
-                    let movieList = response.data;
-                    axios.get('/genre/movie/list' + key)
-                        .then(genreResponse => {
-                            let genreNames = genreResponse.data.genres;
-
-                            for (let movie of movieList.results) {
-                                movie.genres = movie.genre_ids.map(
-                                    id => genreNames.find(
-                                        genre => genre.id === id
-                                    ).name
-                                )
-                            }
-                            if (!this.state.data || (this.state.data && toString(response.data.results) !== toString(this.state.data.results))) {
-                                this.setState({
-                                    data: movieList,
-                                    loading: false,
-                                })
-                            }
+                    let pages  = response.data.total_pages;
+                    // console.log(pageNumber, pages)
+                    if (pageNumber > pages) {
+                        // console.log('[Path by second redirect]', `/movies${path}/page/${pages-1}`)
+                        this.props.history.replace(`/movies${path}/page/${pages-1}`);
+                        this.setState({
+                            loading: true,
                         })
+                    } else {
+                        let movieList = response.data;
+                        axios.get('/genre/movie/list' + key)
+                            .then(genreResponse => {
+                                let genreNames = genreResponse.data.genres;
+
+                                for (let movie of movieList.results) {
+                                    movie.genres = movie.genre_ids.map(
+                                        id => genreNames.find(
+                                            genre => genre.id === id
+                                        ).name
+                                    )
+                                }
+                                if (!this.state.data || (this.state.data && toString(response.data.results) !== toString(this.state.data.results))) {
+                                    this.setState({
+                                        data: movieList,
+                                        loading: false,
+                                    })
+                                }
+                            })
+                        }
                 })
                  
     }
 
     render() {
-        console.log('Loading = ', this.state.loading)
+        // console.log('Loading = ', this.state.loading)
         let displayElement = <Spinner />;
         if (!this.state.loading) {
             let movies = this.state.data.results;
@@ -129,9 +105,10 @@ class MoviesPage extends PureComponent {
                 </div>
             )
             if (movies.length) {
-                displayElement = <Movies data = {movies} />
+                displayElement = <Movies k = {1} data = {movies} />
             }
         }
+        console.log(this.state.data && this.state.data.total_pages)
         return (
             <React.Fragment>
                 <SearchBar
@@ -140,8 +117,20 @@ class MoviesPage extends PureComponent {
                     val = {this.props.match.params.queryName || ''}
                 />
                 {displayElement}
-                <PageSwitcher click={this.switchPage} screenSize = {this.screenSizeY} direction="prev" />
-                <PageSwitcher click={this.switchPage}screenSize = {this.screenSizeY} direction="next" />
+                {this.props.match.params.pageNumber != '1' ?
+                    <PageSwitcher 
+                        click={this.switchPage} 
+                        screenSize = {this.screenSizeY}
+                        direction="prev" 
+                    /> : null
+                }
+                {this.state.data && (this.props.match.params.pageNumber != this.state.data.total_pages ?
+                    <PageSwitcher 
+                        click={this.switchPage}
+                        screenSize = {this.screenSizeY} 
+                        direction="next" 
+                    /> : null)
+                }
             </React.Fragment>
         );
     }
