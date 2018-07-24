66392fe034c49a279ca1a8fb7a5e040d3dc207d4
Alexey Musienko <musaleex@gmail.com>
2018-07-24 16:57:51 +0300

    now can be disabled

--- src/components/PageSwitcher/PageSwitcher.js
+++ src/components/PageSwitcher/PageSwitcher.js
@@ -9,13 +9,14 @@ const pageSwitcher = (props) => {
     let classes = [props.direction === 'next' ? styles.Next : styles.Prev, styles.PageSwitcher].join(' ');
     let path = props.location.pathname, pageNumber = +props.match.params.pageNumber;
     let change = props.direction === 'next' ? 1 : -1;
-    console.log(path)
+    // console.log(path)
+    console.log(props.disabled)
     return (
         <Link to = {path.slice(0,path.lastIndexOf('/') + 1) + (pageNumber + change)}>
             <div 
                 className = {classes} 
                 style = {style}
-                onClick = {props.click}
+                onClick = {!props.disabled ? props.click : null}
             />
         </Link>
     );
