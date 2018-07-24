106299999066f4ba9ed3faf0db2dac2b652e2834
Alexey Musienko <musaleex@gmail.com>
2018-07-24 16:53:32 +0300

    add favorites movies

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,33 +1,67 @@
 import React from 'react';
 import css from './Movie.css';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 const imageNotFound = 'https://static1.squarespace.com/static/541339f0e4b06a3d5343bbf3/590396918419c2bcf19eb8f9/5993484537c581fc38321db8/1502824517922/Image+Coming+Soon.jpg?format=300w';
 
 
-const movie = ({data}) => {
-    let imagePath = data.poster_path ? 'http://image.tmdb.org/t/p/w300' + data.poster_path : imageNotFound;
+const movie = (props) => {
+    let imagePath = props.data.poster_path ? 'http://image.tmdb.org/t/p/w300' + props.data.poster_path : imageNotFound;
+    let style = { transform: `scale(${props.k})` };
+    let isFavorite = props.favorites.indexOf(props.data.id + '') !== -1;
+    let favClass = [css.Favorite, isFavorite ? css.FavoriteAdded : css.FavoriteRemoved].join(' ');
+
+    function localHandler(e) {
+        console.log('click');
+        let isFavorite = props.favorites.indexOf(props.data.id + '') !== -1;
+        console.log(props.favorites);
+        let id = props.data.id + '';
+        if (!isFavorite) {
+            props.onFavAdd(id)
+        } else {
+            props.onFavDel(id)
+        }
+    }
         return (
-        <div className={css.Movie}>
-            <Link to={`/movie/${data.id}`}>
+        <div className={css.Movie} style = {style}>
+            <div 
+                id = {props.data.id} 
+                className = {favClass}
+                onClick = {localHandler}
+            />
+            <Link to={`/movie/${props.data.id}`}>
                 <img 
                     className={css.Card} 
                     src={imagePath} 
-                    alt={data.alt}
+                    alt={props.data.alt}
                 />
             </Link>
             <div className={css.Title}>
-                {(data.title.length > 45 ? data.title.slice(0,45) + '...' : data.title) + 
-                (data.release_date ?  ' (' + data.release_date.slice(0,4) + ')' : '')} 
+                {(props.data.title.length > 45 ? props.data.title.slice(0,45) + '...' : props.data.title) + 
+                (props.data.release_date ?  ' (' + props.data.release_date.slice(0,4) + ')' : '')} 
             </div>
             <div className = {css.Genres}>
-                {data.genres.slice(0,3).join(', ')}
+                {props.data.genres.slice(0,3).join(', ')}
             </div>
             <div className={css.Vote}>
-                {data.vote_average}
+                {props.data.vote_average}
             </div>
         </div>
     );
 }
 
-export default movie;
+const mapStateToProps = (state) => {
+    return {
+        favorites: state.favorites
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onFavAdd: (id) => dispatch({type: 'ADD_FAV', id: id}),
+        onFavDel: (id) => dispatch({type: 'DEL_FAV', id: id})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(movie);
