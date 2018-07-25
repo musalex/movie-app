986dbf7c45683215af711b48f26907047db38cb2
Alexey Musienko <musaleex@gmail.com>
2018-07-25 17:57:01 +0300

    Minor fixes

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,14 +5,14 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch(action.type) {
         case 'ADD_FAV':
-            let newFav = state.favorites.concat([action.id]);
+            let newFav = state.favorites.concat([action.movie]);
             localStorage.setItem('Favorites', JSON.stringify(newFav));
             return {
                 ...state,
                 favorites: newFav
             }
         case 'DEL_FAV':
-            let updatedFav = state.favorites.filter(el=>el!==action.id);
+            let updatedFav = state.favorites.filter(el=>el.id!==action.movie.id);
             localStorage.setItem('Favorites', JSON.stringify(updatedFav));
             return {
                 ...state,
